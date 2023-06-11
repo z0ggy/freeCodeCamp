7f97e15287577dc8f77a25d2502337249d616866
z0ggy <zoggy76@gmail.com>
2023-06-11 18:53:00 +0100

    add constructor

---
+++ tut/main.py
@@ -0,0 +1,26 @@
+class Item:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+    
+    
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+# instantiate object of Item class
+item1 = Item('Phone', 100, 5)
+# print(item1.calculate_total_price(item1.price, item1.quantity))
+print('total price', item1.calculate_total_price())
+# print(item1.name)
+
+item2 = Item('Laptop', 1000, 3)
+# print(item2.calculate_total_price(item2.price, item2.quantity))
+# print(item2.name)
+
+item1.has_audio_jack = False
+# print(item1.has_audio_jack)
+
+# print all attributes for item1 object
+for attr, value in item1.__dict__.items():
+        print(attr, value)
